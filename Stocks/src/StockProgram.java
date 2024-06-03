df7504bd20191b5b81259c838d193159cdc3158c
maggiechua <maggiechua79@gmail.com>
2024-06-03 19:44:20 -0400

    modified main method

--- Stocks/src/StockProgram.java
+++ Stocks/src/StockProgram.java
@@ -4,8 +4,7 @@ public class StockProgram {
   public static void main(String[] args) {
     StockImpl model = new StockImpl();
     Readable rd = new InputStreamReader(System.in);
-    Appendable ap = System.out;
-    StocksController controller = new StocksController(model, rd, ap);
+    StocksController controller = new StocksController(model, rd);
     controller.execute();
   }
 }
--- Stocks/src/StocksController.java
+++ Stocks/src/StocksController.java
@@ -1,5 +1,5 @@
 public class StocksController {
-  public StocksController(StockImpl model, Readable rd, Appendable ap) {
+  public StocksController(StockImpl model, Readable rd) {
 
   }
 
