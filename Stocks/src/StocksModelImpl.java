5f78ee2c577598c128156920bfcbd5a641ee4cd1
Aarushi Shankar <atlas@Aarushis-MacBook-Pro.local>
2024-06-03 18:29:48 -0400

    buy and sell updated

--- Stocks/src/StocksModelImpl.java
+++ Stocks/src/StocksModelImpl.java
@@ -48,11 +48,39 @@ public class StocksModelImpl implements StocksModel {
 
   @Override
   public StocksModelImpl buy(StocksModel stock, Integer shares, String portfolioName) {
-    return null;
+    HashMap<String, HashMap<StocksModel, Integer>> pfs = this.portfolios;
+    HashMap<StocksModel, Integer> currentportfolio = pfs.get(portfolioName);
+    pfs.remove(portfolioName);
+    if (currentportfolio.containsKey(stock)) {
+      currentportfolio.put(stock, currentportfolio.get(stock) + shares);
+    }
+    else {
+      currentportfolio.put(stock, shares);
+    }
+    pfs.put(portfolioName, currentportfolio);
+    return new StocksModelImpl(stock, pfs);
   }
 
   @Override
   public StocksModelImpl sell(StocksModel stock, Integer shares, String portfolioName) {
-    return null;
+    HashMap<String, HashMap<StocksModel, Integer>> pfs = this.portfolios;
+    HashMap<StocksModel, Integer> currentportfolio = pfs.get(portfolioName);
+    pfs.remove(portfolioName);
+    try {
+      if (currentportfolio.containsKey(stock)) {
+        if (currentportfolio.get(stock) >= shares) {
+          currentportfolio.put(stock, currentportfolio.get(stock) - shares);
+        } else {
+          throw new IllegalArgumentException("not enough shares to sell");
+        }
+      } else {
+        throw new IllegalArgumentException("you don't own this stock");
+      }
+    }
+    catch (Exception e) {
+      System.err.println("Not enough shares of this stock in this portfolio to sell.");
+    }
+    pfs.put(portfolioName, currentportfolio);
+    return new StocksModelImpl(stock, pfs);
   }
 }
