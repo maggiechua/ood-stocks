4d75a817cd2b7df6a4419541cf427cee0253d396
maggiechua <maggiechua79@gmail.com>
2024-06-04 19:44:04 -0400

    calculate portfolio method added

---
+++ Stocks/test/StocksModelMock.java
@@ -0,0 +1,44 @@
+import stocks.StocksModel;
+import stocks.StocksModelImpl;
+
+public class StocksModelMock implements StocksModel {
+  @Override
+  public StocksModelImpl stockSelect(String stock) {
+    return null;
+  }
+
+  @Override
+  public Double gainLoss(Integer numOfDays, String date) {
+    return 0.0;
+  }
+
+  @Override
+  public Double movingAvg(Integer numOfDays, String date) {
+    return 0.0;
+  }
+
+  @Override
+  public String crossovers(Integer numOfDays, String date) {
+    return "";
+  }
+
+  @Override
+  public StocksModelImpl createPortfolio(String name) {
+    return null;
+  }
+
+  @Override
+  public StocksModelImpl buy(Integer shares, String portfolioName) {
+    return null;
+  }
+
+  @Override
+  public StocksModelImpl sell(String stock, Integer shares, String portfolioName) {
+    return null;
+  }
+
+  @Override
+  public Double portfolioValue(String portfolioName, String date) {
+    return 0.0;
+  }
+}
