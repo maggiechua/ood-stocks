8ecb5797f963bd5e8b0fa91ee307b3ff303ea975
Aarushi Shankar <atlas@Aarushis-MacBook-Pro.local>
2024-06-04 18:14:25 -0400

    instantializes StocksView in the controller

--- Stocks/src/stocks/StockProgram.java
+++ Stocks/src/stocks/StockProgram.java
@@ -6,8 +6,8 @@ public class StockProgram {
   public static void main(String[] args) {
     StocksModel model = new StocksModelImpl();
     Readable rd = new InputStreamReader(System.in);
-    Appendable ap = System.out;
-    StocksController controller = new StocksControllerImpl(model, rd);
+    StocksView ap = new StocksViewImpl(System.out);
+    StocksController controller = new StocksControllerImpl(model, rd, ap);
     controller.execute();
   }
 }
