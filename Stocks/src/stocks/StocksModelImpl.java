014f8c089ee0e3d0b52e83498522291a3f8c3e32
Aarushi Shankar <atlas@Aarushis-MacBook-Pro.local>
2024-06-18 18:32:19 -0400

    hehe

--- Stocks/src/stocks/StocksModelImpl.java
+++ Stocks/src/stocks/StocksModelImpl.java
@@ -365,7 +365,6 @@ public class StocksModelImpl implements StocksModel {
 
     HashMap<String, Double> barValues = new HashMap<>();
     Double valueGet;
-    String [] date = one.toString().split("-");
     String dateOut;
     valueGet = addingDates(one, time, setValue, isPortfolio , name);
     boolean initial = true;
